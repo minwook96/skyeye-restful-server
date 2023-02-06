24256b8213cc8d1bd29a569bc4db3fea244b0ddf
Minwook Hwang <alsdnr325@gmail.com>
2023-02-06 15:14:31 +0900

    daphne commit

--- server/asgi.py
+++ server/asgi.py
@@ -9,14 +9,16 @@ https://docs.djangoproject.com/en/4.1/howto/deployment/asgi/
 
 import os
 import django
-from django.core.asgi import get_asgi_application
 from django.urls import path, re_path
-from channels.routing import ProtocolTypeRouter, URLRouter
-from channels.auth import AuthMiddlewareStack
-import django_eventstream
-from .token_auth import TokenAuthMiddlewareStack
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "server.settings")
+
+django.setup()
+
+from django.core.asgi import get_asgi_application
+from channels.routing import ProtocolTypeRouter, URLRouter
+import django_eventstream
+from .token_auth import TokenAuthMiddlewareStack
 
 # application = get_asgi_application()
 application = ProtocolTypeRouter({
