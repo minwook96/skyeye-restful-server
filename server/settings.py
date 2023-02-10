56c57acc7a8ca0a272d361f4766b7c557ef42528
Minwook Hwang <alsdnr325@gmail.com>
2023-02-10 14:52:07 +0900

    jazzmin update

--- server/settings.py
+++ server/settings.py
@@ -160,20 +160,20 @@ JAZZMIN_SETTINGS = {
     # # Title on the brand (19 chars max) (defaults to current_admin_site.site_header if absent or None)
     # "site_brand": "Library",
 
-    # Logo to use for your site, must be present in static files, used for brand on top left
-    "site_logo": "/static/img/skysysIcon.ico",
-
-    # Logo to use for your site, must be present in static files, used for login form logo (defaults to site_logo)
-    "login_logo": "/static/img/skysysIcon.ico",
-
-    # Logo to use for login form in dark themes (defaults to login_logo)
-    "login_logo_dark": None,
-
-    # CSS classes that are applied to the logo above
-    "site_logo_classes": "img-circle",
+    # # Logo to use for your site, must be present in static files, used for brand on top left
+    # "site_logo": "/static/img/skysysIcon.ico",
+    #
+    # # Logo to use for your site, must be present in static files, used for login form logo (defaults to site_logo)
+    # "login_logo": "/static/img/skysysIcon.ico",
+    #
+    # # Logo to use for login form in dark themes (defaults to login_logo)
+    # "login_logo_dark": None,
+    #
+    # # CSS classes that are applied to the logo above
+    # "site_logo_classes": "img-circle",
 
     # Relative path to a favicon for your site, will default to site_logo if absent (ideally 32x32 px)
-    "site_icon": "/static/img/skysysIcon.ico",
+    "site_icon": "/img/skysysIcon.ico",
 
     # # Welcome text on the login screen
     # "welcome_sign": "Welcome to the library",
